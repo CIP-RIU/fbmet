58e5afea19d5ac0e598571f5e33fd9c47f1a212a
Frojast <rsimon64@gmail.com>
2016-08-22 08:55:47 -0500

    remove about

--- R/met_ui.R
+++ R/met_ui.R
@@ -18,10 +18,11 @@ met_ui <- function(title = "MET explorer"){
           ),
           tabPanel("Help",
                    helpPanel(fbhelp::list_tutorials("fbmet")[[1]])
-          ),
-          tabPanel("About",
-                   helpPanel(fbhelp::list_tutorials("fbmet", name = "about")[[1]],
-                             center = TRUE)
           )
+          # ,
+          # tabPanel("About",
+          #          helpPanel(fbhelp::list_tutorials("fbmet", name = "about")[[1]],
+          #                    center = TRUE)
+          # )
   )
 }
